c6237a7ecb9812953edd93a26c9b98710395fc0b
Connie <leungcnie@gmail.com>
2021-01-07 02:58:04 +0000

    Add instance variables to Dashboard controller and update admin/dashboard view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['HTTP_USERNAME'], password: ENV['HTTP_PASS']
   
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
