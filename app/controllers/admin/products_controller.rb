51305aa0c70dc82eb1b4b0539a53030e15b7b483
Connie <leungcnie@gmail.com>
2021-01-07 01:59:55 +0000

    Add HTTP basic authentication using environment variables

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV['HTTP_USERNAME'], password: ENV['HTTP_PASS']
+  
   def index
     @products = Product.order(id: :desc).all
   end
