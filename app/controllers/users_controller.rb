5f2e3f47d540f2dcf106f4727e1963d74e443bdd
Connie <leungcnie@gmail.com>
2021-01-08 01:30:50 +0000

    Add users controller for handling sign up and registering new users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  #renders signup form
+  def new
+  end
+
+  #receiving form + create user with form params
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
