f2bf37eeb591093fbef16fa7639aeaff18a887fb
Connie <leungcnie@gmail.com>
2021-01-08 05:12:49 +0000

    Add logic for sale status in Sale model and render in sale erb partial

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,17 @@
 class Sale < ActiveRecord::Base
+
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    #starts_on == self.starts_on
+    starts_on > Date.current
+  end
+
+  def active?
+    #return is implicit
+    !upcoming? && !finished?
+  end
+
 end
