34fbe96a70704a7396e5a0bca78a208c022eac91
Connie <leungcnie@gmail.com>
2021-01-12 07:00:12 +0000

    Add downcasing + leading/trailing spaces removal to email and add new first and last name fields to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   #receiving form + create user with form params
   def create
     user = User.new(user_params)
+    user.email = user_params[:email].downcase.strip
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
